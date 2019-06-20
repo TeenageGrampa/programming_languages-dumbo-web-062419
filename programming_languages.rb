48429932642f1b50859d0b43441d84b87c1371e9
Daniel Voigt <danielhvoigt@gmail.com>
2019-06-20 15:54:07 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,5 +4,11 @@ def reformat_languages(languages)
   languages.each do |style, description|
     description.each do |lang, type|
       if new_hash.has_key?(lang)
-        
+        new_hash[lang][:style] << style
+      else
+        new_hash[lang] = type
+        new_hash[lang][:style] = [style]
+      end
+    end
+    
 end
